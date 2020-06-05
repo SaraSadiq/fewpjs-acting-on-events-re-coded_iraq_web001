80ab0f6c3a9b4e09a96f00ae0dbce368b593deda
Sara Nooraldin <sarasaadiq98@gmail.com>
2020-06-05 02:26:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Your code here
 function functionName() {
   let dodger = document.getElementById("dodger");
-  dodger.style.backgroundColor = "#000000";
+  dodger.style.backgroundColor = "#001000";
 }
