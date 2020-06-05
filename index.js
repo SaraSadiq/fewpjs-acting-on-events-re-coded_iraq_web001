844ee350adc5c23d3939a0e84ecb43199591789b
Sara Nooraldin <sarasaadiq98@gmail.com>
2020-06-05 02:35:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,5 +2,4 @@
 
   let dodger = document.getElementById("dodger");
   dodger.style.backgroundColor = "#FF69B4";
-  console.log(dodger.style.left:"180px"); // "180px"
-  console.log(dodger.style.bottom);
+dodger.style.bottom = "100px";
