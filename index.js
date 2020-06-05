e47949504534f7582685821442ade4deb0a2d64b
Sara Nooraldin <sarasaadiq98@gmail.com>
2020-06-05 02:50:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,16 +13,17 @@ document.addEventListener("keydown", function(e) {
   }
 });
 let dodger = document.getElementById("dodger");
-function moveDodgerLeft() {
-  let leftNumbers = dodger.style.left.replace("px", "");
-  let left = parseInt(leftNumbers, 10);
 
-  if (left > 0) {
-    dodger.style.left = `${left + 1}px`;
-  }
-}
 document.addEventListener("keydown", function(e) {
   if (e.key === "ArrowLeft") {
+    function moveDodgerLeft() {
+      let leftNumbers = dodger.style.left.replace("px", "");
+      let left = parseInt(leftNumbers, 10);
+
+      if (left > 0) {
+        dodger.style.left = `${left + 1}px`;
+      }
+    }
     moveDodgerLeft(){
 
     }
